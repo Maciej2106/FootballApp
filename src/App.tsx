1298e82c6061a4be7331813199c504e7208576b1
Maciej <rozek@o2.pl>
2025-01-07 13:20:43 +0100

    backgrondBody and add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,49 @@
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { ButtonBody } from "./backgroundBody/ButtonBody";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const GlobalStyle = createGlobalStyle`
+body{
+background-color: ${(props) => props.theme.colors.background}
+}
+`;
+
 export const App = () => {
-    return null;
+  const [isLight, setLight] = useState(true);
+
+  const light = {
+    colors: {
+      primary: "0333EE",
+      textPrimary: "#fff",
+      background: "#eee",
+      textBackground: "#333",
+    },
+  };
+
+  const dark = {
+    colors: {
+      primary: "0333EE",
+      textPrimary: "#222",
+      background: "#333",
+      textBackground: "#eee",
+    },
+  };
+
+  const toggleTheme = () => {
+    setLight((prevLight) => !prevLight);
+  };
+
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={isLight ? light : dark}>
+          <ButtonBody label="Change body" onClick={toggleTheme} />
+          <GlobalStyle />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </>
+  );
 };
---
+++ src/backgroundBody/ButtonBody.tsx
@@ -0,0 +1,32 @@
+import styled from'styled-components'
+
+type ButtonProps ={
+    label: string;
+    onClick: () => void;
+
+}
+
+const StyledButton = styled.button`
+    border: none;
+    padding: 20px;
+    min-width: 150px;
+    width: 10%;
+    background-color: #399b52ed;
+    color:rgb(12, 11, 11);
+    border: 3px solid #b5a9a9;
+    margin: 2% auto;
+
+    &:hover {
+    box-shadow: 5px 2px 7px 7px #233467ed;
+    }
+`
+
+export const ButtonBody = ({label, onClick} : ButtonProps) =>{
+    return (
+        <>        
+        <StyledButton onClick={onClick}>
+            {label}
+        </StyledButton>
+        </>
+    )
+}
